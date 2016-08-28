d7242fd3e6831326d0263b3ea9adcc5fa53266af
Hank Andre <hankandre@gmail.com>
2016-08-28 07:21:43 -0500

    added temp/ directory to .gitignore

--- temp/js/controllers/EditInfoController.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-userApp.controller('EditInfoController', ['$scope', 'UserService', function ($scope, UserService) {
-  $scope.getStrava = function () {
-    UserService.getStrava();
-  };
-}]);
--- temp/js/templates/templates.js
+++
@@ -1,3 +0,0 @@
-angular.module('app').run(['$templateCache', function($templateCache) {$templateCache.put('/public/app/authentication/authentication.html','<p>Hello {{auth.test}}</p>');
-$templateCache.put('/public/app/home/home.html','<p>Hello {{home.test}}</p>');
-$templateCache.put('/public/app/registration/registration.html','<p>Hello {{reg.test}}</p>');}]);
